c2f3953f0dd32488f219005649b7697de988381d
Siddhant Singh <siddhant06137@gmail.com>
2026-03-14 03:07:46 +0530

    type any fix

--- apps/web/type.d.ts
+++ apps/web/type.d.ts
@@ -1,6 +1,6 @@
+import type * as React from 'react';
+
 declare module '*.svg' {
-    import React = require('react');
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
+    const content: React.FC<React.SVGProps<SVGSVGElement>>;
+    export default content;
 }
