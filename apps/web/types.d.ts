1e927f5d5d7f8e44fcc6c39bb58f46318380fdb6
Siddhant Singh <siddhant06137@gmail.com>
2026-04-03 03:45:25 +0530

    fix image types

--- apps/web/types.d.ts
+++ apps/web/types.d.ts
@@ -3,3 +3,28 @@ declare module '*.svg' {
     const content: any;
     export default content;
 }
+
+declare module '*.png' {
+    const content: import('next/image').StaticImageData;
+    export default content;
+}
+
+declare module '*.jpg' {
+    const content: import('next/image').StaticImageData;
+    export default content;
+}
+
+declare module '*.jpeg' {
+    const content: import('next/image').StaticImageData;
+    export default content;
+}
+
+declare module '*.gif' {
+    const content: import('next/image').StaticImageData;
+    export default content;
+}
+
+declare module '*.webp' {
+    const content: import('next/image').StaticImageData;
+    export default content;
+}
